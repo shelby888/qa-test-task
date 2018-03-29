349c7a6bf1e59ec716afa1992b7ced619d60d242
Ivan Subbotin <shelby888@yandex.ru>
2018-03-29 19:17:03 +0300

    Add test to open New Post page

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -4,8 +4,14 @@ describe 'Posts spec' do
   let(:posts_page) { PostPage.new }
   let(:post) { create :post }
 
-  it 'Reset password for enabled user' do
+  it 'Open posts list' do
     posts_page.load
     expect(posts_page).to have_header
+  end
+
+  it 'Open new post page' do
+    posts_page.load
+    click_link 'New Post'
+    expect(find('h1')).to have_content('New Post')
   end
 end
